b895a50bb4b216fa74e62f011de1a289f842c2ae
Reahill <2018484796@qq.com>
2026-03-15 16:34:02 +0800

    katex trail

--- site/js/katex.js
+++ site/js/katex.js
@@ -1,16 +1,10 @@
-document$.subscribe(() => {
-  const scope = document.querySelector('.md-content');
-  if (!scope) return;
-
-  renderMathInElement(scope, {
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
     delimiters: [
       { left: "$$",  right: "$$",  display: true },
       { left: "$",   right: "$",   display: false },
       { left: "\\(", right: "\\)", display: false },
       { left: "\\[", right: "\\]", display: true }
     ],
-    ignoredTags: ["script", "noscript", "style", "textarea", "pre", "code", "option"],
-    throwOnError: false,
-    strict: "ignore"
   })
 })
